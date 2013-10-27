1023d0108bbc209d1e65734ff8b438fcf0d8ce18
Rob Salmond <rsalmond@appneta.com>
2013-10-26 18:36:54 -0700

    basic player template view rendering

--- foosweb/app.py
+++ foosweb/app.py
@@ -22,11 +22,18 @@ def home():
     assets.register('js', Bundle(main_js, 'js/foosview.js'))
     return render_template('foosview.html')#, debug_image='static/img/table.png')
 
-@app.route('/player')
-def player():
+@app.route('/player/')
+@app.route('/player/<id>')
+def player(id=-1):
     assets.register('css', main_css)
     assets.register('js', main_js)
-    return render_template('player.html')
+
+    gw = GameWatch()
+    player_info = {}
+    player_info['name'] = gw.GetNameByID(id)
+    player_info['id'] = id
+
+    return render_template('player.html', **player_info)
 
 @app.route('/history')
 def live_hist():
