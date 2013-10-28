c651c62974a1b5085dd6e56e73d785acbd87d7dc
Rob Salmond <rsalmond@appneta.com>
2013-10-28 00:09:10 -0700

    fix busted test script

--- foosweb/test/fuzzy.sh
+++ foosweb/test/fuzzy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 4
-curl -X POST -H "Content-Type: application/json" -d '@lessplayers.json' http://localhost:5000/players
+curl -X POST -H "Content-Type: application/json" -d '@lessplayers.json' http://localhost:5000/current_players
