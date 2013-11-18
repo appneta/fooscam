cbb509f63f22a19caa83d0183da54d11e34df5d5
Rob Salmond <rsalmond@appneta.com>
2013-11-17 17:20:42 -0800

    add 'prod' config

--- foosweb/config.py
+++ foosweb/config.py
@@ -21,6 +21,16 @@ class Dev(Config):
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
 
+class Prod(Config):
+    SERVER_NAME= 'foosserv'
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_TLS = bool(os.getenv('MAIL_USE_TLS'))
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
 class Test(Config):
     DEBUG = True
     TESTING = True
